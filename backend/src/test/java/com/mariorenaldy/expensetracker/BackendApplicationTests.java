0ef24c6c9561862b88ffca5b481acdab1badf57d
Mario Renaldy <mariorenaldyy@gmail.com>
2026-07-31 19:09:49 +0700

    chore: add docker compose for postgres and scaffold spring boot backend

---
+++ backend/src/test/java/com/mariorenaldy/expensetracker/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mariorenaldy.expensetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
